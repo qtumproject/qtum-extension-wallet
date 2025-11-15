70dbb1d14414b16842835dea6456019ef4c8025c
meherett <meherett.batu@gmail.com>
2025-11-15 03:59:39 +0300

    Refactor: renamed all UI extensions to tsx

---
+++ packages/snap/src/helpers/ui/index.tsx
@@ -0,0 +1,41 @@
+import { Component, DialogType, OnHomePageHandler, panel } from "@metamask/snaps-sdk";
+import { QtumWallet } from "qtum-ethers-wrapper";
+
+import { getWallet } from "@/config";
+import { renderDashboard, renderHome } from "@/helpers";
+import { getQtumAddress } from "@/helpers/format";
+
+export * from './dashboard';
+export * from './home';
+export * from './tx';
+export * from './wallet-creation';
+
+export const getSnapDialog = async (type: DialogType, content: Component[]) => {
+  return snap.request({
+    method: 'snap_dialog',
+    params: {
+      type,
+      content: panel(content),
+    },
+  });
+};
+
+export const onHomePage: OnHomePageHandler = async () => {
+  let wallet: QtumWallet;
+  let hexAddress: string | undefined;
+  let qtumAddress: string | undefined;
+
+  try {
+    wallet = await getWallet();
+    hexAddress = wallet.address;
+    qtumAddress = await getQtumAddress();
+  } catch (_) { }
+
+  const id = await snap.request({
+    method: 'snap_createInterface',
+    params: {
+      ui: hexAddress ? renderDashboard({ qtumAddress, hexAddress }) : renderHome(),
+    },
+  });
+  return { id };
+};
