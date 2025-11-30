f3534a5b54f453abebbf709c29c72ba0d9581eee
meherett <meherett.batu@gmail.com>
2025-11-30 23:16:52 +0300

    Update: DEFAULT_NETWORKS_RPC_URLS

--- packages/snap/src/consts/networks.ts
+++ packages/snap/src/consts/networks.ts
@@ -2,20 +2,6 @@ import type { Chain } from '@qtumproject/qtum-wallet-connector';
 
 export const DEFAULT_NETWORKS_RPC_URLS: Chain[] = [
   {
-    chainId: '8889',
-    chainName: 'Testnet',
-    rpcUrls: [
-      'https://testnet.qnode.qtum.info/v1/aPaVeuBrYtFZ6oyKQZlNqSUSjrHjCy5v26IYX',
-    ],
-    iconUrls: [''],
-    nativeCurrency: {
-      name: 'TQtum',
-      symbol: 'TQtum',
-      decimals: 8,
-    },
-    blockExplorerUrls: ['https://testnet.qtum.info/'],
-  },
-  {
     chainId: '81',
     chainName: 'Mainnet',
     rpcUrls: [
@@ -24,9 +10,23 @@ export const DEFAULT_NETWORKS_RPC_URLS: Chain[] = [
     iconUrls: [''],
     nativeCurrency: {
       name: 'Qtum',
-      symbol: 'Qtum',
+      symbol: 'QTUM',
       decimals: 8,
     },
     blockExplorerUrls: ['https://qtum.info/'],
+  },
+  {
+    chainId: '8889',
+    chainName: 'Testnet',
+    rpcUrls: [
+      'https://testnet.qnode.qtum.info/v1/aPaVeuBrYtFZ6oyKQZlNqSUSjrHjCy5v26IYX',
+    ],
+    iconUrls: [''],
+    nativeCurrency: {
+      name: 'TQtum',
+      symbol: 'TQTUM',
+      decimals: 8,
+    },
+    blockExplorerUrls: ['https://testnet.qtum.info/'],
   },
 ];
